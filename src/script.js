692cb644e01ee7282d9c9d89ab4361dedee563d4
fannykai <139573774+fannykai@users.noreply.github.com>
2024-02-10 19:52:35 +0100

    connected the date and time to the api and created a new function in order to display the date correctly

--- src/script.js
+++ src/script.js
@@ -6,11 +6,15 @@ function refreshWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
-  console.log(response.data);
 
   //den här raden göra att när man byter ut h1:an i html koden, men har skivit staden med olika stora bokstäver kommer den att skira ut standen som den är skriven i api datan
   cityElement.innerHTML = response.data.city;
+
+  // hämtar date och tid från apiet (vi kalla rpå funktionen under som gör om datumet till mänsklig variant)
+  timeElement.innerHTML = formatDate(date);
 
   //den här raden lägger in beskrivning av vänder där id:et description finns i Html konden
   descriptionElement.innerHTML = response.data.condition.description;
@@ -18,10 +22,34 @@ function refreshWeather(response) {
   // hämtar humidity från apiet
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 
+  // hämtar wind speed från apiet
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
 
   //här tar avrundar jag temperaturen
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+// det här är en funktion som jag om datumet till männsklig version.
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  //den här gör så att det adderas en noll om minuterna är mellan 0-9
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 //Under är funktionen som tar emot värdet av searchInput.valu för att veta vilke data den ska hämta och ge rätt temperatur
